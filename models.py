d12ccdacbdf052b49840b731cd0df29d3e5cc389
arvinder004 <asdhoul004@gmail.com>
2025-12-09 13:06:10 +0530

    added input output and intermediate model structure

--- models.py
+++ models.py
@@ -1,22 +1,42 @@
 from pydantic import BaseModel, Field
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Literal
 
+# Input
 class ProductData(BaseModel):
     name: str
-    concentration: str
+    concentration: Optional[str] = None
     skin_type: List[str]
     ingredients: List[str]
     benefits: List[str]
     how_to_use: str
-    side_effects: str
-    price: int
+    side_effects: Optional[str] = None
+    price: float
 
-class Section(BaseModel):
+
+#Intermediate
+class UserQuestion(BaseModel):
+    category: Literal['Informational', 'Safety', 'Usage', 'Purchase', 'Comparison']
+    question_text: str
+    answer_text: Optional[str] = None
+
+class CompetitorProduct(BaseModel):
+    name: str
+    key_ingredients: List[str]
+    benefits: List[str]
+    price: float
+
+class AgentState(BaseModel):
+    primary_product: ProductData
+    competitor_product: Optional[CompetitorProduct] = None
+    generated_questions: List[UserQuestion] = []
+
+
+# Output
+class PageSection(BaseModel):
     heading: str
     content: str | List[Dict]
 
 class PageOutput(BaseModel):
     page_type: str
-    sections: List[Section]
+    sections: List[PageSection]
     meta_tags: List[str]
-    
